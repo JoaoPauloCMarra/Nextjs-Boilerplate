94dec7d1964f454c6c7ad63fbe2963b829d58f3e
John Marra <joaopaulocmarra@gmail.com>
2023-12-28 09:57:05 -0300

    change brand logo icon

--- src/components/brand-logo.tsx
+++ src/components/brand-logo.tsx
@@ -1,9 +1,16 @@
-import homeSVG from '@/icons/home.svg';
+import Image from 'next/image';
+import Icon from '@/icons/brand.svg';
 
 export const BrandLogo = () => (
 	<div className="flex flex-col items-center">
 		<div className="rounded-full bg-gradient-to-r from-cyan-500 to-blue-500 p-4">
-			<img src={homeSVG.src} className="svg-white size-10" alt="Home Icon" />
+			<Image
+				src={Icon.src}
+				className="svg-white size-10"
+				alt="Home Icon"
+				height={Icon.height}
+				width={Icon.width}
+			/>
 		</div>
 	</div>
 );
