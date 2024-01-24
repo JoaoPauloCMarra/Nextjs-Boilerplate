a9ef88d425d294a370b2ef9789e6690e05b8678c
John Marra <joaopaulocmarra@gmail.com>
2024-01-24 08:22:59 -0300

    playwright webserver config

--- playwright.config.ts
+++ playwright.config.ts
@@ -40,13 +40,11 @@ export default defineConfig({
 			],
 
 	/* Run your local dev server before starting the tests */
-	webServer: process.env.CI
-		? undefined
-		: {
-				command: 'bun start',
-				url: 'http://127.0.0.1:3000',
-				reuseExistingServer: !process.env.CI
-			},
+	webServer: {
+		command: 'bun start',
+		url: 'http://127.0.0.1:3000',
+		reuseExistingServer: !process.env.CI
+	},
 
 	/* Configure projects for major browsers */
 	projects: [
