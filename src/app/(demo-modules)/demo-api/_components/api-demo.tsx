a2c6f9e2a9f64154c3390d926a52fe788360c44c
John Marra <joaopaulocmarra@gmail.com>
2024-02-20 09:21:28 -0300

    Add dynamic loading and error handling to DemoExternalAPI component

---
+++ src/app/(demo-modules)/demo-api/_components/api-demo.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { useState } from 'react';
+import JSONViewer from '@/components/json-viewer';
+import LoadingIndicator from '@/components/loading-indicator';
+import { Button } from '@/components/primitives/button';
+
+type ResponseState = {
+	data: unknown | Record<string, unknown>;
+	errorMessage: string;
+	loading?: boolean;
+};
+
+type Props = {
+	url: string;
+	initialState?: ResponseState;
+	fetchData: (
+		id?: number
+	) => Promise<{ data: unknown | Record<string, unknown>; errorMessage: string }>;
+};
+
+const ApiDemo = (props: Props) => {
+	const [{ data, errorMessage, loading }, setResponse] = useState<ResponseState>(
+		props.initialState ?? {
+			data: null,
+			errorMessage: '',
+			loading: false
+		}
+	);
+
+	const onRefetch = async () => {
+		setResponse((current) => ({ ...current, errorMessage: '', loading: true }));
+		try {
+			const response = await props.fetchData(Math.floor(Math.random() * 20) + 1);
+			setResponse({ data: response, errorMessage: '', loading: false });
+		} catch (error) {
+			setResponse({ data: {}, errorMessage: String(error), loading: false });
+		}
+	};
+
+	return (
+		<div className="container flex w-full flex-1 flex-col items-center gap-4 py-4">
+			<h2 className="text-2xl">Demo api response inside a RSC with 2s wait</h2>
+			<p className="italic opacity-70">data from: {props.url}</p>
+			<div className="w-full max-w-sm overflow-x-auto rounded-xl bg-slate-700 p-1 md:p-4">
+				{loading ? (
+					<div className="flex w-full items-center justify-center">
+						<LoadingIndicator />
+					</div>
+				) : null}
+				{!loading && !errorMessage && data ? <JSONViewer data={data} /> : null}
+				{!loading && errorMessage && <code className="text-sm text-red-500">{errorMessage}</code>}
+			</div>
+			<Button onClick={onRefetch}>refetch with different id</Button>
+		</div>
+	);
+};
+
+export default ApiDemo;
--- src/app/(demo-modules)/demo-api/page.tsx
+++ src/app/(demo-modules)/demo-api/page.tsx
@@ -1,7 +1,14 @@
 import type { Metadata } from 'next';
 import dynamic from 'next/dynamic';
 import { baseMetadata } from '@/lib/constants';
+import { waitSeconds } from '@/lib/utils';
 import DemoAPISkeleton from './_components/skeleton';
+
+const ApiDemo = dynamic(() => import('./_components/api-demo'), {
+	loading: () => {
+		return <DemoAPISkeleton />;
+	}
+});
 
 export const metadata: Metadata = {
 	...baseMetadata,
@@ -9,18 +16,19 @@ export const metadata: Metadata = {
 	description: ''
 };
 
-const DATA_URL = 'https://jsonplaceholder.typicode.com/users/1';
-const JSONViewer = dynamic(() => import('@/components/json-viewer'), {
-	loading: () => {
-		return <DemoAPISkeleton />;
-	}
-});
+const DATA_URL = 'https://jsonplaceholder.typicode.com/users';
 
-export default async function DemoExternalAPI() {
+const fetchData = async (id?: number) => {
+	'use server';
+
 	let data = undefined;
 	let errorMessage: string = '';
 
-	const response = await fetch(DATA_URL);
+	if (!id) {
+		return { data, errorMessage: 'id is required' };
+	}
+
+	const response = await fetch(`${DATA_URL}/${id}`);
 
 	try {
 		if (!response.ok) {
@@ -32,14 +40,30 @@ export default async function DemoExternalAPI() {
 		errorMessage = String(error);
 	}
 
+	await waitSeconds(1);
+
+	return { data, errorMessage };
+};
+
+export default async function DemoExternalAPI() {
+	let data = undefined;
+	let errorMessage: string = '';
+
+	try {
+		const response = await fetchData(1);
+		data = response.data;
+	} catch (error) {
+		errorMessage = String(error);
+	}
+
 	return (
-		<div className="container flex w-full flex-1 flex-col items-center gap-4 py-4">
-			<h2 className="text-2xl">Demo api response inside a RSC with 2s wait</h2>
-			<p className="italic opacity-70">data from: {DATA_URL}</p>
-			<div className="w-full max-w-sm overflow-x-auto rounded-xl bg-slate-700 p-1 md:p-4">
-				{!errorMessage && data && <JSONViewer data={data} />}
-				{errorMessage && <code className="text-sm text-red-500">{errorMessage}</code>}
-			</div>
-		</div>
+		<ApiDemo
+			url={DATA_URL}
+			fetchData={fetchData}
+			initialState={{
+				data,
+				errorMessage
+			}}
+		/>
 	);
 }
